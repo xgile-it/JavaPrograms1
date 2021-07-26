17b83f2c34ee22422d4c62f0517e51ce294acd3a
tusharsingh27 <46473956+tusharsingh27@users.noreply.github.com>
2021-07-26 22:58:45 +0530

    Add files via upload

---
+++ FileHandling/FileWrite.java
@@ -0,0 +1,26 @@
+package FileHandling;
+
+import java.io.FileWriter;
+
+public class FileWrite
+{
+        public static void main(String args[])
+        {
+
+            String data = "This is the data in the output file";
+            try {
+                // Creates a Writer using FileWriter
+                FileWriter output = new FileWriter("output.txt");
+
+                // Writes string to the file
+                output.write(data);
+                System.out.println("Data is written to the file.");
+
+                // Closes the writer
+                output.close();
+            }
+            catch (Exception e) {
+                e.getStackTrace();
+            }
+        }
+    }

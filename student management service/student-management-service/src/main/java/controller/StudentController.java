7225eebcd834d3f5830de13d4b13042ce69ea6ee
tusharsingh27 <46473956+tusharsingh27@users.noreply.github.com>
2021-09-04 13:56:09 +0530

    This Project is about Student Management System.

--- student management service/student-management-service/src/main/java/controller/StudentController.java
+++ student management service/student-management-service/src/main/java/controller/StudentController.java
@@ -61,7 +61,7 @@ public class StudentController {
         existingStudent.setLastName(student.getLastName());
         existingStudent.setEmail(student.getEmail());
 
-        // save updated student object
+        // save update student object
         studentService.updateStudent(existingStudent);
         return "redirect:/students";
     }

c6d1b8cdfc76a4f3e5c1d63ad19c7f8fa43807a9
tusharsingh27 <46473956+tusharsingh27@users.noreply.github.com>
2021-09-04 13:53:38 +0530

    This Project is about Student Management System.

--- student management service/student-management-service/src/main/java/service/StudentService.java
+++ student management service/student-management-service/src/main/java/service/StudentService.java
@@ -1,11 +1,8 @@
 package service;
 
 import java.util.List;
-
-//import entity.Student;
 import entity.Student;
 //import sms.entity.Student;
-//import net.javaguides.sms.entity.Student;
 
 public interface StudentService {
     List<Student> getAllStudents();

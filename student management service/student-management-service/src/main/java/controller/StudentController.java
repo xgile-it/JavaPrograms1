1dfd16b40b6489e5693bc513730f276fd4b1bf32
tusharsingh27 <46473956+tusharsingh27@users.noreply.github.com>
2021-09-11 19:23:21 +0530

    This Project is about Student Management System

--- student management service/student-management-service/src/main/java/controller/StudentController.java
+++ student management service/student-management-service/src/main/java/controller/StudentController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-//import sms.entity.Student;
 import service.StudentService;
 
 @Controller

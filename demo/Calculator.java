4cc0cf26002ba40e2fb9807cffb32abb2ccabd1f
tusharsingh27 <46473956+tusharsingh27@users.noreply.github.com>
2021-11-10 20:24:32 +0530

    This project is about Book Implementation.

---
+++ demo/Calculator.java
@@ -0,0 +1,14 @@
+package com.booksimplementation.demo;
+
+public class Calculator {
+
+    //sum
+    public int doSum(int a,int b,int c){
+        return a+b+c;
+    }
+
+    //product
+    public int doProduct(int a,int b){
+        return a*b;
+    }
+}
